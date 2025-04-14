50e75b67b5fc92e9ef5522660418f1b9037456f7
spadaritucks <thiago.spadari02@gmail.com>
2025-04-13 23:01:45 -0300

    Determinação de Layouts

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,13 +1,22 @@
+import { Pizza } from "lucide-react";
 import { Outlet } from "react-router-dom";
 
 
 export function AuthLayout() {
 
     return (
-        <div>
-            <h1>Autenticação</h1>
+        <div className="min-h-screen grid grid-cols-2 ">
+            <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
+                <div className="flex items-center gap-3 text-lg font-medium text-foreground">
+                    <Pizza className="h-5 w-5" />
+                    <span className="font-semibold">Pizza Shop</span>
+                </div>
+                <footer className="text-sm">
+                    Painel do Parceiro &copy; Pizza Shop - {new Date().getFullYear()}
+                </footer>
+            </div>
 
-            <div>
+            <div className="flex flex-col justify-center items-center relative">
                 <Outlet />
             </div>
         </div>
