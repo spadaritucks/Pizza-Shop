82472d7bf50cb9d0481560599d59fa0e76f8dd5d
spadaritucks <thiago.spadari02@gmail.com>
2025-04-13 22:57:12 -0300

    Criação das rotas e determinação da pagina not found

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,33 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Dashboard } from "./pages/app/dashboard";
-import { SignIn } from "./pages/app/sign-in";
+import { Dashboard } from "./pages/app/dashboard/dashboard";
+import { SignIn } from "./pages/auth/sign-in";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
+import { SignUp } from "./pages/auth/sign-up";
+import { Orders } from "./pages/app/orders/orders";
+import { NotFound } from "./pages/404";
 
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <AppLayout />,
-        children: [{ path: '/', element: <Dashboard /> }]
+        errorElement: <NotFound/>,
+        children: 
+        [
+            { path: '/', element: <Dashboard /> },
+            { path: '/orders', element: <Orders /> }
+        ]
     },
     {
-        path: '/sign-in',
+        path: '/',
         element: <AuthLayout />,
-        children: [{ path: '/sign-in', element: <SignIn /> }]
+        children:
+            [
+                { path: '/sign-in', element: <SignIn /> },
+                { path: '/sign-up', element: <SignUp /> }
+            ],
+
     },
+
 ])
