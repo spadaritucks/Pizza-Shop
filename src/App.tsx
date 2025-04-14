53afc1ffde09d35fd62e4263258fd256bdcc6bf1
spadaritucks <thiago.spadari02@gmail.com>
2025-04-13 22:56:42 -0300

    Instalação do Sonner e React Helmet e Aplicação de Estilos Globais

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import { router } from './routes'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 function App() {
-  
+
+
 
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | Pizza Shop" />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey='pizzashop-theme' defaultTheme='dark'>
+        <Helmet titleTemplate="%s | Pizza Shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
