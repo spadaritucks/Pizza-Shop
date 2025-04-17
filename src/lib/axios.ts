63a8e8413126c830195203ceff3d9da27d5f1ff8
spadaritucks <thiago.spadari02@gmail.com>
2025-04-17 11:41:48 -0300

    Adição de arquivos axios.ts e react-query.ts para configuração da API e gerenciamento de consultas com React Query.

---
+++ src/lib/axios.ts
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+})
+
+if(import.meta.env.VITE_ENABLE_API_DELAY){
+    api.interceptors.request.use(async (config) => {
+        await new Promise(resolve => setTimeout(resolve, 2000))
+        return config
+    })
+}
