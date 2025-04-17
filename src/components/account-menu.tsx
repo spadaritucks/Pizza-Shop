a13013f8e681a019facc2a7ca30fddd707cd9bab
spadaritucks <thiago.spadari02@gmail.com>
2025-04-17 11:41:02 -0300

    Implementação do menu de conta com integração de perfil, gerenciamento de restaurante e funcionalidade de logout. Adição do componente de seleção de intervalo de datas e do componente de status de pedidos. Criação do diálogo para edição do perfil da loja.

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,34 +1,79 @@
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
-import { Building, ChevronDown } from "lucide-react";
+import { Building, ChevronDown, LogOut } from "lucide-react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getProfile } from "../api/get-profile";
+import { getManagedRestaurant } from "../api/get-managed-restaurant";
+import { Skeleton } from "./ui/skeleton";
+import { Dialog, DialogTrigger } from "./ui/dialog";
+import { StoreProfileDialog } from "./store-profile-dialog";
+import { signOut } from "../api/sign-out";
+import { useNavigate } from "react-router-dom";
 
 
-export function AccountMenu () {
+export function AccountMenu() {
 
-    return(
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="outline" className="flex items-center gap-2 select-none">
-                    Pizza Shop
-                    <ChevronDown className="w-4 h-4" />
-                </Button>
-            </DropdownMenuTrigger>
+    const navigate = useNavigate()
+    const { data: profile, isLoading: isLoadingProfile } = useQuery({
+        queryKey: ['profile'],
+        queryFn: getProfile,
+        staleTime: Infinity,
+    })
 
-            <DropdownMenuContent align="end" className="w-56">
-                <DropdownMenuLabel className="flex flex-col">
-                    <span>Diego Fernandes</span>
-                    <span className="text-xs font-normal text-muted-foreground ">diego@rocketseat.com.br</span>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator/>
-                <DropdownMenuItem>
-                    <Building className="mr-2 w-4 h-4" />
-                    <span>Perfil da Loja</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-                    <Building className="mr-2 w-4 h-4 " />
-                    <span>Sair</span>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+    const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } = useQuery({
+        queryKey: ['managed-restaurant'],
+        queryFn: getManagedRestaurant,
+        staleTime: Infinity,
+
+    })
+
+    const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+        mutationFn: signOut,
+        onSuccess: () => {
+            navigate('/sign-in', { replace: true })
+        }
+
+    })
+
+    return (
+        <Dialog>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline" className="flex items-center gap-2 select-none">
+                        {isLoadingManagedRestaurant ? <Skeleton className="h-4 w-40" /> : managedRestaurant?.name}
+                        <ChevronDown className="w-4 h-4" />
+                    </Button>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent align="end" className="w-56">
+                    <DropdownMenuLabel className="flex flex-col">
+                        {isLoadingProfile ? (<div className="space-y-1.5">
+                            <Skeleton className="h-4 w-32" />
+                            <Skeleton className="h-3 w-24" />
+                        </div>) : <>
+                            <span>{profile?.name}</span>
+                            <span className="text-xs font-normal text-muted-foreground ">{profile?.email}</span>
+                        </>}
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DialogTrigger asChild>
+                        <DropdownMenuItem >
+                            <Building className="mr-2 w-4 h-4" />
+                            <span>Perfil da Loja</span>
+                        </DropdownMenuItem>
+                    </DialogTrigger>
+
+                    
+                    <DropdownMenuItem asChild className="text-rose-500 dark:text-rose-400" disabled={isSigningOut}>
+                        <button className="w-full" onClick={() => signOutFn()}>
+                            <LogOut className="mr-2 h-4 w-4" />
+                            <span>Sair</span>
+                        </button>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <StoreProfileDialog />
+        </Dialog>
+
     )
 }
--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,16 +1,17 @@
 import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from "lucide-react";
 import { Button } from "./ui/button";
 
-export interface PaginationProps{
-    pageIndex : number;
+export interface PaginationProps {
+    pageIndex: number;
     totalCount: number;
     perPage: number;
+    onPageChange: (pageIndex: number) => Promise<void> | void
 }
 
-export function Pagination({pageIndex, perPage, totalCount} : PaginationProps){
-    const pages = Math.ceil(totalCount/perPage) || 1
+export function Pagination({ pageIndex, perPage, totalCount, onPageChange }: PaginationProps) {
+    const pages = Math.ceil(totalCount / perPage) || 1
 
-    return(
+    return (
         <div className="flex items-center justify-between">
             <span className="text-sm text-muted-foreground">
                 Total de {totalCount} item(s)
@@ -19,20 +20,20 @@ export function Pagination({pageIndex, perPage, totalCount} : PaginationProps){
                 <div className="font-medium text-sm">Pagina {pageIndex + 1} de {pages}</div>
             </div>
             <div className="flex items-center gap-2">
-                <Button variant="outline" className="h-8 w-8 ">
-                    <ChevronsLeft className="h-4 w-4"/>
+                <Button onClick={() => onPageChange(0)} variant="outline" className="h-8 w-8 " disabled={pageIndex === 0} >
+                    <ChevronsLeft className="h-4 w-4" />
                     <span className="sr-only">Primeira Pagina</span>
                 </Button>
-                <Button variant="outline" className="h-8 w-8 ">
-                    <ChevronLeft className="h-4 w-4"/>
-                    <span className="sr-only">Proxima Pagina</span>
-                </Button>
-                <Button variant="outline" className="h-8 w-8 ">
-                    <ChevronRight className="h-4 w-4"/>
+                <Button onClick={() => onPageChange(pageIndex - 1)} variant="outline" className="h-8 w-8 " disabled={pageIndex === 0}>
+                    <ChevronLeft className="h-4 w-4" />
                     <span className="sr-only">Pagina Anterior</span>
                 </Button>
-                <Button variant="outline" className="h-8 w-8 ">
-                    <ChevronsRight className="h-4 w-4"/>
+                <Button onClick={() => onPageChange(pageIndex + 1)} variant="outline" className="h-8 w-8 " disabled={pages <= pageIndex + 1}>
+                    <ChevronRight className="h-4 w-4" />
+                    <span className="sr-only">Proxima Pagina</span>
+                </Button>
+                <Button onClick={() => onPageChange(pages - 1)} variant="outline" className="h-8 w-8 " disabled={pages <= pageIndex + 1}>
+                    <ChevronsRight className="h-4 w-4" />
                     <span className="sr-only">Ultima Pagina</span>
                 </Button>
             </div>
